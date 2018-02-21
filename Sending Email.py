c35574b34e581a855d291dc7a5c626b6e83510e7
akshitchandora <36480340+akshitchandora@users.noreply.github.com>
2018-02-22 00:46:50 +0530

    Update Sending Email.py

--- Sending Email.py
+++ Sending Email.py
@@ -6,7 +6,7 @@ Outlook.com/Hotmail.com: smtp-mail.outlook.com
 Yahoo Mail:smtp.mail.yahoo.com'''
 conn.ehlo() #Hello Method To Connect To SMTP Server
 conn.starttls() #TLS Encryption For Password Before You Can Login In
-conn.login('Here Your Gmail','Here is Your Password') # Email And Password
+conn.login('Here is Your Gmail','Here is Your Password') # Email And Password
 '''You Can Use Google App Password Generator For Password - It is Easier to generate password for your different devices. 
 Here is Link -> https://security.google.com/settings/security/apppasswords'''
 conn.sendmail('Here is Sender Email','Here is Receiver Email','Subject: <Here is Subject>\n\n <Body>')
